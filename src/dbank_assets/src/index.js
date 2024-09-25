8396f1e51dc29ca6ab80f4a1c5b7e00be0902f3e
a-levitt <artemis.levitt@gmail.com>
2024-09-25 14:40:28 +0200

    Connected motoko backend to frontend. Work on topUp and withdraw funcs

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -4,4 +4,16 @@ import { dbank } from "../../declarations/dbank/";
 window.addEventListener("load", async () => {
     const currentAmount = await dbank.checkBalance();
     document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
-})
+});
+
+document.querySelector("form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const inputAmount = parseFloat(document.getElementById("input-amount").value);
+    const withdrawAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+    await dbank.topUp(inputAmount);
+
+    const currentAmount = await dbank.checkBalance();
+    document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+});
