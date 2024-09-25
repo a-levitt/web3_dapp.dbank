fc06a71d95a14f3a451d477f6ffd2ac0ac9a8062
a-levitt <artemis.levitt@gmail.com>
2024-09-25 14:21:29 +0200

    Connected motoko backend to frontend. Current balance

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,1 +1,7 @@
 
+import { dbank } from "../../declarations/dbank/";
+
+window.addEventListener("load", async () => {
+    const currentAmount = await dbank.checkBalance();
+    document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+})
